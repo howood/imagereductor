94c13503746b67025cee67d7396c9f25dfc71a3b
howood <>
2025-11-20 20:10:53 +0900

    fix image validate

--- application/validator/imagevalidator.go
+++ application/validator/imagevalidator.go
@@ -55,16 +55,23 @@ func NewImageValidator(imagetype []string, maxwidth, maxheight, maxfilesize int)
 //
 //nolint:cyclop
 func (val *ImageValidator) Validate(ctx context.Context, uploadfile io.Reader) error {
-	imageinfo, format, err := image.DecodeConfig(uploadfile)
+	// Use TeeReader to read file data once for both DecodeConfig and size validation
+	buf := new(bytes.Buffer)
+	teeReader := io.TeeReader(uploadfile, buf)
+
+	// Decode config from tee reader (reads and writes to buffer simultaneously)
+	imageinfo, format, err := image.DecodeConfig(teeReader)
 	log.Debug(ctx, fmt.Sprintf("%#v", imageinfo))
 	log.Debug(ctx, format)
 	if err != nil {
-		return fmt.Errorf("%w", err)
+		return fmt.Errorf("decode image config: %w", err)
 	}
+
 	//nolint:err113
 	if !utils.StringArrayContains(val.imagetype, format) {
 		return fmt.Errorf("invalid Image type: %s", strings.Join(val.imagetype, "/"))
 	}
+
 	sizeerrormsg := make([]string, 0)
 	if val.maxwidth != 0 && imageinfo.Width > val.maxwidth {
 		sizeerrormsg = append(sizeerrormsg, fmt.Sprintf("Over Image width: %d px", val.maxwidth))
@@ -72,14 +79,17 @@ func (val *ImageValidator) Validate(ctx context.Context, uploadfile io.Reader) e
 	if val.maxheight != 0 && imageinfo.Height > val.maxheight {
 		sizeerrormsg = append(sizeerrormsg, fmt.Sprintf("Over Image height: %d px", val.maxheight))
 	}
-	buf := new(bytes.Buffer)
-	if _, err := buf.ReadFrom(uploadfile); err != nil {
-		return err
+
+	// Read remaining data if DecodeConfig didn't consume all
+	if _, err := io.Copy(buf, teeReader); err != nil {
+		return fmt.Errorf("read remaining data: %w", err)
 	}
+
+	filesize := buf.Len()
 	log.Debug(ctx, val.maxfilesize)
 	log.Debug(ctx, float64(val.maxfilesize)/1024/1024, 2)                   //nolint:mnd
 	log.Debug(ctx, utils.RoundFloat(float64(val.maxfilesize)/1024/1024, 2)) //nolint:mnd
-	if val.maxfilesize != 0 && buf.Len() > val.maxfilesize {
+	if val.maxfilesize != 0 && filesize > val.maxfilesize {
 		//nolint:mnd
 		sizeerrormsg = append(sizeerrormsg, fmt.Sprintf("Over Image filesize: %v MB", utils.RoundFloat(float64(val.maxfilesize)/1024/1024, 2)))
 	}
